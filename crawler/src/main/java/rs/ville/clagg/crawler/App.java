57d09d10975358dd153b2677d89546d857faa464
Cameron Villers <cameron@ville.rs>
2013-07-29 23:20:13 -0400

    Load config, DB connection manager

--- crawler/src/main/java/rs/ville/clagg/crawler/App.java
+++ crawler/src/main/java/rs/ville/clagg/crawler/App.java
@@ -1,13 +1,47 @@
 package rs.ville.clagg.crawler;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	private static final Logger log = LogManager.getLogger(App.class);
+	
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        Properties props = new Properties();
+        String configPath;
+        
+        if(System.getProperty("clagg.crawlerconfig") == null)
+        {
+        	configPath = "crawler.properties";
+        }
+        else
+        {
+        	configPath = System.getProperty("clagg.crawlerconfig");
+        }
+        
+        try
+        {
+        	props.load(new FileInputStream(configPath));
+        }
+        catch(FileNotFoundException ex)
+        {
+        	log.fatal("Could not find crawler.properties", ex);
+        } 
+        catch(IOException ex)
+        {
+			log.fatal("Exception while loading crawler.properties", ex);
+		}
+        
+        DBConnectionManager.init(props);
     }
 }
---
+++ crawler/src/main/java/rs/ville/clagg/crawler/DBConnectionManager.java
@@ -0,0 +1,55 @@
+package rs.ville.clagg.crawler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.postgresql.Driver;
+
+public class DBConnectionManager
+{
+	private static final Logger log = LogManager.getLogger(DBConnectionManager.class);
+	
+	private static String host;
+	private static Boolean ssl;
+	private static Integer port;
+	private static String dbName;
+	private static String username;
+	private static String password;
+	
+	public static void init(Properties props)
+	{
+		host = props.getProperty("db.host");
+		ssl = new Boolean(props.getProperty("db.ssl"));
+		port = new Integer(props.getProperty("db.port"));
+		
+		dbName = props.getProperty("db.name");
+		username = props.getProperty("db.username");
+		password = props.getProperty("db.password");
+		
+		try
+		{
+			Class.forName("org.postgresql.Driver");
+		}
+		catch(ClassNotFoundException ex)
+		{
+			log.fatal("Could not find PostgreSQL JDBC class", ex);
+		}
+	}
+	
+	private static Connection getConnection() throws SQLException
+	{
+		String url = String.format("jdbc:postgresql://%s:%i/%s", host, port, dbName);
+		
+		Properties props = new Properties();
+		
+		props.setProperty("user", username);
+		props.setProperty("password", password);
+		props.setProperty("ssl", ssl.toString());
+		
+		return DriverManager.getConnection(url, props);
+	}
+}
