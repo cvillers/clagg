dceefa8ff7d22035adf2df328847ef590c25afd9
Cameron Villers <cameron@ville.rs>
2013-10-05 13:35:03 -0400

    Add .NET project for downloading and initial program structure

---
+++ dotnet/Clagg/Downloader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Npgsql;
+
+namespace Downloader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			NpgsqlConnection connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["ClaggDB"].ConnectionString);
+
+			connection.Open();
+		}
+	}
+}
