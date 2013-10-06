e6553713b21eda10c028df556a32b45ec8fb143a
Cameron Villers <cameron@ville.rs>
2013-10-05 23:18:25 -0400

    Initial downloader program with sample NYC data

--- dotnet/Clagg/Downloader/DbConnection.cs
+++ dotnet/Clagg/Downloader/DbConnection.cs
@@ -84,7 +84,7 @@ namespace Downloader
 			{
 				Direction = ParameterDirection.Input,
 				ParameterName = "i_job_id",
-				NpgsqlDbType = NpgsqlDbType.Smallint,
+				NpgsqlDbType = NpgsqlDbType.Bigint,
 				Value = Id
 			});
 
@@ -93,6 +93,162 @@ namespace Downloader
 			tx.Commit();
 
 			connection.Close();
+		}
+
+		public long GetUndownloadedListingCount(bool IncludeDeleted, string UrlFilter)
+		{
+			long ret = 0;
+
+			var connection = GetConnection();
+
+			var tx = connection.BeginTransaction();
+			NpgsqlCommand cmd = new NpgsqlCommand("listing_get_undownloaded_count", connection);
+
+			cmd.CommandType = CommandType.StoredProcedure;
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = "include_deleted",
+				NpgsqlDbType = NpgsqlDbType.Boolean,
+				Value = IncludeDeleted
+			});
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = "url_filter",
+				NpgsqlDbType = NpgsqlDbType.Varchar,
+				Value = UrlFilter
+			});
+
+			var reader = cmd.ExecuteReader();
+
+			while(reader.Read())
+				ret = reader.GetInt64(0);
+
+			reader.Close();
+
+			tx.Commit();
+
+			connection.Close();
+
+			return ret;
+		}
+
+		public DataTable GetUndownloadedListings(bool IncludeDeleted, long PageSize, long PageNumber)
+		{
+			DataSet ret = new DataSet();
+
+			var connection = GetConnection();
+
+			var tx = connection.BeginTransaction();
+			NpgsqlCommand cmd = new NpgsqlCommand("listing_get_undownloaded", connection);
+
+			cmd.CommandType = CommandType.StoredProcedure;
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = "include_deleted",
+				NpgsqlDbType = NpgsqlDbType.Boolean,
+				Value = IncludeDeleted
+			});
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = "c_listings",
+				NpgsqlDbType = NpgsqlDbType.Refcursor,
+				Value = "listings"
+			});
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = "page_size",
+				NpgsqlDbType = NpgsqlDbType.Bigint,
+				Value = PageSize
+			});
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = "page_number",
+				NpgsqlDbType = NpgsqlDbType.Bigint,
+				Value = PageNumber
+			});
+
+			NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(cmd);
+
+			adapter.Fill(ret);
+
+			tx.Commit();
+
+			connection.Close();
+
+			return ret.Tables[0];
+		}
+
+		public DataTable GetUndownloadedListingsWithLocation(bool IncludeDeleted, string UrlFilter, long PageSize, long PageNumber)
+		{
+			DataSet ret = new DataSet();
+
+			var connection = GetConnection();
+
+			//var tx = connection.BeginTransaction();
+
+			StringBuilder sql = new StringBuilder("SELECT * from listing_undownloaded_with_location ");
+
+			string clause = "WHERE";
+
+			if(!IncludeDeleted)
+			{
+				sql.Append("WHERE deleted_on IS NULL ");
+				clause = "AND";
+			}
+
+			sql.Append(clause + " url LIKE :url_filter ");
+
+			sql.Append("OFFSET :page_number * :page_size LIMIT :page_size");
+
+			NpgsqlCommand cmd = new NpgsqlCommand(sql.ToString(), connection);
+
+			cmd.CommandType = CommandType.Text;
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = ":url_filter",
+				NpgsqlDbType = NpgsqlDbType.Varchar,
+				NpgsqlValue = '%' + UrlFilter + '%'
+			});
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = ":page_number",
+				NpgsqlDbType = NpgsqlDbType.Bigint,
+				Value = PageSize
+			});
+
+			cmd.Parameters.Add(new NpgsqlParameter
+			{
+				Direction = ParameterDirection.Input,
+				ParameterName = ":page_size",
+				NpgsqlDbType = NpgsqlDbType.Bigint,
+				Value = PageNumber
+			});
+
+			NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(cmd);
+
+			adapter.Fill(ret);
+
+			//tx.Commit();
+
+			connection.Close();
+
+			return ret.Tables[0];
 		}
 	}
 }
--- dotnet/Clagg/Downloader/Program.cs
+++ dotnet/Clagg/Downloader/Program.cs
@@ -2,23 +2,88 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 using Npgsql;
 
+using OfficeOpenXml;
+
 namespace Downloader
 {
 	class Program
 	{
+		private const int PageSize = 1000;
+
+		private static Dictionary<string, string> Areas = new Dictionary<string, string>
+		{
+			{ "mnh", "Manhattan" },
+			{ "brk", "Brooklyn" },
+			{ "que", "Queens" }
+		};
+
+		static void Download(DbConnection db, string UrlFilter, ExcelWorksheet sheet)
+		{
+			long total = db.GetUndownloadedListingCount(false, UrlFilter);
+
+			sheet.Cells["A1"].Value = "ID";
+			sheet.Cells["B1"].Value = "Title";
+			sheet.Cells["C1"].Value = "Price";
+			sheet.Cells["C1"].StyleName = "Currency";
+			sheet.Cells["D1"].Value = "URL";
+			sheet.Cells["E1"].Value = "Latitude";
+			sheet.Cells["F1"].Value = "Longitude";
+			sheet.Cells["G1"].Value = "Address";
+
+			int rc = 2;
+
+			for(long page = 0; page <= total / PageSize; page++)
+			{
+				DataTable listings = db.GetUndownloadedListingsWithLocation(false, UrlFilter, PageSize, page);
+
+				foreach(var row in listings.AsEnumerable())
+				{
+					sheet.Cells[rc, 1].Value = row["id"];
+					sheet.Cells[rc, 2].Value = row["title"];
+					sheet.Cells[rc, 3].Value = row["price"];
+					sheet.Cells[rc, 4].Value = row["url"];
+					sheet.Cells[rc, 5].Value = row["lat"];
+					sheet.Cells[rc, 6].Value = row["long"];
+
+					rc++;
+				}
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			if(args.Length < 1)
+			{
+				Console.WriteLine("Usage: {0} <filename.xlsx>", Assembly.GetExecutingAssembly().GetName());
+
+				return;
+			}
+
 			DbConnection db = new DbConnection();
 
 			var id = db.CreateJob(JobTypes.Download);
 
 			Console.WriteLine("New job id {0}", id);
+
+			using(ExcelPackage package = new ExcelPackage(new FileInfo(args[0])))
+			{
+				foreach(var area in Areas)
+				{
+					ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(area.Value);
+
+					Download(db, area.Key, worksheet);
+				}
+
+				package.Save();
+			}
 
 			db.FinishJob(id);
 		}
